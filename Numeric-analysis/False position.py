7540d97593c6db88e33d7e34bfa9c17293e63b0b
JoseSholly <95097621+JoseSholly@users.noreply.github.com>
2023-06-03 20:10:41 +0100

    Python file

--- Numeric-analysis/False position.py
+++ Numeric-analysis/False position.py
@@ -7,32 +7,33 @@ def interval(equation):
     # Finding Interval
     equation = sympify(equation)
     L1, L2 = [], []
-    for i in range(-10, 10):
-        ans = equation.subs({x: f"{i}"})
+    for i in range(0, 10):
+        ans = equation.subs({'x': f"{i}"})
         if ans < 0:
             L1 += [i]
 
-    for i in range(L1[-1], 10):
-        ans = equation.subs({x: f"{i}"})
+    for i in range(0, 10):
+        ans = equation.subs({'x': f"{i}"})
         if ans > 0:
             L2 += [i]
-            # print(ans, i)
-            # break
-    return L1[-1], L2[0]
-
+    
+    if L2[0]>L1[-1]:
+        return (L1[-1], L2[0])
+    else:
+        return (L2[-1], L1[0] )
 
 def is_continuous(equation):
     # For any continuous function
 
-    x1 = interval(equation)[0]
-    x2 = interval(equation)[1]
+    v1 = interval(equation)[0]
+    v2 = interval(equation)[1]
 
     # Substititing intervals into function
-    fxa = equation.subs({x: x1})
-    fxb = equation.subs({x: x2})
+    fxa = equation.subs({"x": v1})
+    fxb = equation.subs({"x": v2})
 
     # Find two points, say a and b such that a<b and f(a)*f(b)=0
-    if x1 < x2 and (fxa*fxb) < 0:
+    if v1 < v2 and (fxa*fxb) < 0:
         return True
     else:
         return False
@@ -49,8 +50,9 @@ def false_position(equation, E=0.0001):
 
             x0, x1 = a, b
             equation = sympify(equation)
-            fxa = equation.subs({x: x0})
-            fxb = equation.subs({x: x1})
+            fxa = equation.subs({'x': x0})
+            fxb = equation.subs({'x': x1})
+
             root = 0.1
             i = 0
             x2 = 0
@@ -64,9 +66,9 @@ def false_position(equation, E=0.0001):
                 p1 += [round(x0, 5)]
                 p2 += [round(x1, 5)]
                 Xo += [round(x2, 5)]
-                
+
                 #  Substituting mid-point into f(x)-equation
-                root = equation.subs({x: x2}).n(5)
+                root = equation.subs({'x': x2}).n(5)
                 fx_Xo += [round(root, 5)]
 
                 # It is the root of the given function if f(t)=0, else follow the step:
@@ -84,13 +86,14 @@ def false_position(equation, E=0.0001):
                 else:
                     x0, x1 = x2
 
-                fxa = equation.subs({x: x0})
+                fxa = equation.subs({'x': x0})
                 fx_p1 += [round(fxa, 5)]
-                fxb = equation.subs({x: x1})
+                fxb = equation.subs({'x': x1})
                 fx_p2 += [round(fxb, 5)]
-            d = {"x0": p1, "f(x0)": fx_p1, "x1": p2, "f(x1)": fx_p2, "x2": Xo,
+
+            dict_answer = {"x0": p1, "f(x0)": fx_p1, "x1": p2, "f(x1)": fx_p2, "x2": Xo,
                  "f(x2)": fx_Xo, "Point": point, "root": Xo[-1], "iteration": len(p1)}
-            return d
+            return dict_answer
         except:
             raise Exception("Function is not continous!!!")
     else:
@@ -99,13 +102,13 @@ def false_position(equation, E=0.0001):
 
 # print(false_position(x**3-x-1))
 
-# print(false_position(2*x**2-3))
+# print(false_position(2*x**2 - 3))
 
-# print(false_position(2*x**3 - 2*x -5))
+# print(false_position(2*x**3 - 2*x-5))
 
 # print(false_position(x**2 - 12))
 
 # print(false_position(x**3-48, 0.0001))
 
-
 # print(false_position(x**3 + 2*x**2 + x -1, 0.0001))
+
