2c282f26964ecc3cad3ddb42422525707c131a13
JoseSholly <95097621+JoseSholly@users.noreply.github.com>
2023-06-03 20:06:56 +0100

    Python File

--- Numeric-analysis/Bisection.py
+++ Numeric-analysis/Bisection.py
@@ -5,45 +5,46 @@ x,y = symbols('x y')
 
 def interval(equation):
     # Finding Interval
-    equation= sympify(equation)
-    L1, L2=[],[]
-    for i in range(-10,10):
-        ans=equation.subs({x:f"{i}"})
-        if ans<0:
-            L1+=[i]
-            # print(ans, i)
-            #break
-   
-    for i in range(L1[-1],10):
-        ans=equation.subs({x:f"{i}"})
-        if ans>0:
-            L2+=[i]
-            # print(ans, i)
-            #break
-    return L1[-1],L2[0]
+    equation = sympify(equation)
+    L1, L2 = [], []
+    for i in range(0, 10):
+        ans = equation.subs({x: f"{i}"})
+        if ans < 0:
+            L1 += [i]
+
+    for i in range(0, 10):
+        ans = equation.subs({x: f"{i}"})
+        if ans > 0:
+            L2 += [i]
+    
+    if L2[0]>L1[-1]:
+        return (L1[-1], L2[0])
+    else:
+        return (L2[-1], L1[0] )
 
 
 def is_continuous(equation):
     # For any continuous function
-    
-    x1= interval(equation)[0]
-    x2=interval(equation)[1]
+
+    v1 = interval(equation)[0]
+    v2 = interval(equation)[1]
 
     # Substititing intervals into function
-    fxa=equation.subs({x:x1})
-    fxb=equation.subs({x:x2})
+    fxa = equation.subs({"x": v1})
+    fxb = equation.subs({"x": v2})
+    # print(fxa, fxb)
 
     # Find two points, say a and b such that a<b and f(a)*f(b)=0
-    if x1<x2 and (fxa*fxb)<0:
+    if v1 < v2 and (fxa*fxb) < 0:
         return True
     else:
         return False
-      
+
+
 def bisection(equation,E=0.0001):
     
     if is_continuous(equation):
         try:
-
             # Find the value of f(x) at a=1 and a=2
             x1= interval(equation)[0]
             x2=interval(equation)[1]
@@ -52,8 +53,6 @@ def bisection(equation,E=0.0001):
             equation= sympify(equation)
             fxa=equation.subs({x:a})
             fxb=equation.subs({x:b})
-            # print(fxa)
-            # print(fxb)
     
             t= 0.1
             i=0
@@ -68,17 +67,17 @@ def bisection(equation,E=0.0001):
                 p1+=[round(a, 5)]
                 p2+=[round(b, 5)]
                 Xo+=[round(mid_point, 5)]
-                # print(f"X{i-1}= {mid_point} Interval btw {a} and {b}")
+                
                 #  Substituting mid-point into f(x)-equation
                 t= equation.subs({x:mid_point}).n(5)
                 fx_Xo+=[round(t,5)]
-                # print(t)
 
                 fxa=equation.subs({x:a})
                 fx_p1+=[round(fxa, 5)]
                 fxb=equation.subs({x:b})
                 fx_p2+=[round(fxb, 5)]
-                
+
+
                 # It is the root of the given function if f(t)=0, else follow the step:
                 # Divide the interval [a, b] - if f(t)*f(a)<0, there exist a root between t and a. Then, mid-point(Xo)= b
                 # if f(t)*f(b)<0, there exist a root between t and b. Then, mid-point(Xo)= a
@@ -86,21 +85,16 @@ def bisection(equation,E=0.0001):
                     if t*fxa<0:
                         b= mid_point
                         point+=["b"]
-
-                        
+                       
                     elif t*fxb<0:
                         a=mid_point
                         point+=["a"]
 
-                    # print("f(x{})= {}\n".format(i-1,t))
-
                 else:
                     a,b= mid_point
 
-
-            d= {"a": p1,"f(a)": fx_p1, "b":p2,"f(b)":fx_p2, "mid_point":Xo, "f(mid_point)": fx_Xo, "Point":point, "root": Xo[-1], "iteration":len(p1)}
-            
-            return d 
+            dict_answer= {"a": p1,"f(a)": fx_p1, "b":p2,"f(b)":fx_p2, "mid_point":Xo, "f(mid_point)": fx_Xo, "Point":point, "root": Xo[-1], "iteration":len(p1)}
+            return dict_answer
         except:
             raise Exception("Function is not continous!!!")
     else:
@@ -110,17 +104,20 @@ def bisection(equation,E=0.0001):
         
 # bisection(equation, tolerance(if given))
 
-# bisection(x**3-x-1)
+# bisection(((x**3)-x-1), 0.001)
 
-# bisection(2*x**2-3)
+# bisection(((2*x**2)-3), 0.001)
 
-# bisection(2*x**3-2*x-5)
+# bisection(((2*x**3)-(2*x)-5))
 
-# bisection(x**2-12)
+# bisection((x**2)-12, 0.001)
 
-# bisection(x**3-48)
+# print(bisection((x**3)-48, 0.0001))
 
-# print(bisection(x**3 + 2*x**2 + x -1)
+# print(bisection(((x**3) + (2*x**2) + x -1), 0.0001))
+
+# print(bisection(2 + sin(x) - x ))
+
 
 
     
